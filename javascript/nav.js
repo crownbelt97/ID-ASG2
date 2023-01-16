5646b09e18ecba97d1590f16814293b38e2e6f1d
crownbelt97 <s10247153@connect.np.edu.sg>
2023-01-16 12:50:31 +0800

    Added Navigation Bar
    Implemented Navigation Bar for:
    - Navigation bar content items
    - Dropdown Navigation bar when responsive
    - Hamburger Menu Icon
    - Close Hamburger Menu Icon

---
+++ javascript/nav.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+
+//Choosing primary navigation
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// Menu shows on/off when clicked
+navToggle.addEventListener('click', () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+	
+	
+});
