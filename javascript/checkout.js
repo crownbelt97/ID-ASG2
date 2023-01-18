7c19cfd4a32b89c5378a20c99e00f0b2fa2f8822
crownbelt97 <s10247153@connect.np.edu.sg>
2023-01-18 18:59:50 +0800

    Added Checkout Page
    - Shipping Details
    - Payment Details
    - Review

---
+++ javascript/checkout.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+const pages = document.querySelectorAll(".page");
+    const translateAmount = 100; 
+    let translate = 0;
+
+    slide = (direction) => {
+
+      direction === "next" ? translate -= translateAmount : translate += translateAmount;
+
+      pages.forEach(
+        pages => (pages.style.transform = `translateX(${translate}%)`)
+      );
+    }
